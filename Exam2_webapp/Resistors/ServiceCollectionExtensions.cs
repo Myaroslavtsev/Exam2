5e47dfbef0dd73a0c0dbe0db2e823988511c2e91
Myaroslavtsev <myaroslavtsev@yandex.ru>
2022-03-23 22:07:42 +0700

    Resistors service collection extensions added

--- Exam2_webapp/Resistors/ServiceCollectionExtensions.cs
+++ Exam2_webapp/Resistors/ServiceCollectionExtensions.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Model;
+using Model.Resistors;
+using MongoDB.Driver;
 
 namespace Exam2_webapp.Resistors
 {
-    public class ServiceCollectionExtensions
+    internal static class ServiceCollectionExtensions
     {
+        public static IServiceCollection AddResistors(this IServiceCollection services)
+        {
+            services.AddSingleton(provider =>
+            {
+                var configuration = provider.GetRequiredService<IConfiguration>();
+                var connectionString = configuration["ConnectionString"];
+                var client = new MongoClient(connectionString);
+                var database = client.GetDatabase("Components");
+                var collection = database.GetCollection<Resistor>("Resistors");
+                return collection;
+            });
+            services.AddSingleton<IResistorsRepository, ResistorsRepository>();
+            services.AddSingleton<ResistorsService>();
+
+            return services;
+        }
     }
 }
