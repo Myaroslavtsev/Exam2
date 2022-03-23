16a299ee32f211b5531c3819a21cb4996aaaac5a
Myaroslavtsev <93481828+Myaroslavtsev@users.noreply.github.com>
2022-03-23 17:59:41 +0700

    ResistorsRepository stub added

--- Model/ResistorsRepository.cs
+++ Model/ResistorsRepository.cs
@@ -2,12 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using View.Resistors;
 
 namespace Model
 {
     public class ResistorsRepository : IResistorsRepository
     {
+        public Task<Resistors.Resistor> CreateResistorAsync(ResistorCreateInfo createinfo, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task DeleteResistorAsync(string id, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Resistors.Resistor> GetResistorAsync(string id, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Resistors.Resistor>> SearchResisrosAsync(ResistorSearchInfo searchinfo, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateResistorAsync(string id, ResistorUpdateInfo updateInfo, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
