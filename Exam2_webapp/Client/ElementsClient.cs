fcb958764710e037e690027be9f82258e122d057
Myaroslavtsev <93481828+Myaroslavtsev@users.noreply.github.com>
2022-03-27 23:44:03 +0700

    Client stup added

---
+++ Exam2_webapp/Client/ElementsClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using View;
+using System.Threading;
+using System.Threading.Tasks;
+using View.Resistors;
+
+namespace Exam2_webapp.Client
+{
+    public sealed class ElementsClient : IElementsClient
+    {
+        // Post("")
+        public async Task<Resistor> CreateResistorAsync(ResistorCreateInfo createInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Get("{id}")
+        public async Task<Resistor> GetResistorAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Get("")
+        public async Task<Resistor> SearchResistorAsync(ResistorSearchInfo searchInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Delete("{id}")
+        public async Task DeleteResistorAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Patch("{id}")
+        public async Task PatchResistorAsync(string id, ResistorUpdateInfo updateInfo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Exam2_webapp/Client/IElementsClient.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using View;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Exam2_webapp.Client
+{
+    public interface IElementsClient 
+    {
+        // Post("")
+        Task<View.Resistors.Resistor> CreateResistorAsync(View.Resistors.ResistorCreateInfo createInfo);
+
+        // Get("{id}")
+        Task<View.Resistors.Resistor> GetResistorAsync(string id);
+
+        // Get("")
+        Task<View.Resistors.Resistor> SearchResistorAsync(View.Resistors.ResistorSearchInfo searchInfo);
+
+        // Delete("{id}")
+        Task DeleteResistorAsync(string id);
+
+        // Patch("{id}")
+        Task PatchResistorAsync(string id, View.Resistors.ResistorUpdateInfo updateInfo);
+    }
+}
